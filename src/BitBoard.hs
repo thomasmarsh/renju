a528a17f835db679e16ce7733b347d149a432172
Thomas Marsh <tmarsh@spotify.com>
2018-08-15 00:52:34 -0400

    DRY up with ConstraintKinds

--- src/BitBoard.hs
+++ src/BitBoard.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE PolyKinds             #-}
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE MagicHash             #-}
+{-# LANGUAGE ConstraintKinds     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE MagicHash           #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module BitBoard
     ( BitBoard(..)
@@ -47,8 +48,9 @@ height _ = fromIntegral $ natVal' (proxy# :: Proxy# h)
 toInt :: BitBoard w h a -> a
 toInt (BitBoard m) = m
 
-instance (KnownNat w, KnownNat h, Bits a, Num a)
-    => Bits (BitBoard w h a) where
+type BitBoardT w h a = (KnownNat w, KnownNat h, Bits a, Integral a)
+
+instance BitBoardT w h a => Bits (BitBoard w h a) where
     (.&.)                 = applyBin (.&.)
     (.|.)                 = applyBin (.|.)
     xor                   = applyBin xor
@@ -66,8 +68,7 @@ instance (KnownNat w, KnownNat h, Bits a, Num a)
     popCount (BitBoard m) = popCount m
     bit i                 = BitBoard (bit i)
 
-instance (KnownNat w, KnownNat h, Bits a, Num a)
-    => FiniteBits (BitBoard w h a) where
+instance BitBoardT w h a => FiniteBits (BitBoard w h a) where
     finiteBitSize x = width x * height x
 
 -- The following methods naively assume that any operation they are
@@ -77,7 +78,7 @@ instance (KnownNat w, KnownNat h, Bits a, Num a)
 -- raw values.
 
 applyBin
-    :: (KnownNat w, KnownNat h, Bits a, Num a)
+    :: BitBoardT w h a
     => (a -> a -> a)
     -> BitBoard w h a
     -> BitBoard w h a
@@ -87,24 +88,21 @@ applyBin f b@(BitBoard m1)
     = BitBoard . maskToSize b $ f m1 m2
 {-# INLINE applyBin #-}
 
-applyUn :: (KnownNat w, KnownNat h, Bits a, Num a)
+applyUn :: BitBoardT w h a
         => (a -> a) -> BitBoard w h a -> BitBoard w h a
 applyUn f b@(BitBoard m) = BitBoard . maskToSize b $ f m
 {-# INLINE applyUn #-}
 
-applyS :: (KnownNat w, KnownNat h, Bits a, Num a)
+applyS :: BitBoardT w h a
        => (a -> b -> a) -> BitBoard w h a -> b -> BitBoard w h a
 applyS f b@(BitBoard m) x = BitBoard . maskToSize b $ f m x
 {-# INLINE applyS #-}
 
-maskToSize :: (KnownNat w, KnownNat h, Bits a, Num a)
-           => BitBoard w h a -> a -> a
+maskToSize :: BitBoardT w h a => BitBoard w h a -> a -> a
 maskToSize b m = m .&. (2 ^ finiteBitSize b - 1)
 {-# INLINE maskToSize #-}
 
-instance (KnownNat h, KnownNat w, Bits a, Integral a, Show a)
-    => Show (BitBoard w h a) where
-
+instance (BitBoardT w h a, Show a) => Show (BitBoard w h a) where
     show b@(BitBoard m)
         = unlines . map (intersperse ' ') $ rows
         where binStr  = showIntAtBase 2 intToDigit m ""
@@ -113,7 +111,7 @@ instance (KnownNat h, KnownNat w, Bits a, Integral a, Show a)
               rows    = chunksOf (width b) (pad ++ binStr)
 
 -- |A row of 1s along each wall
-wall :: (KnownNat w, KnownNat h, Bits a, Num a)
+wall :: BitBoardT w h a
      => Direction -> (Int, Int) -> BitBoard w h a
 wall N (w,h) = BitBoard ((2^w-1) `shift` (w * (h-1)))
 wall E (w,h) = BitBoard (foldl (.|.) 0
@@ -129,13 +127,13 @@ wall NW z = wall N z .|. wall W z
 wall SE z = wall S z .|. wall E z
 wall SW z = wall S z .|. wall W z
 
-shiftD' :: (KnownNat w, KnownNat h, Bits a, Num a)
+shiftD' :: BitBoardT w h a
         => BitBoard w h a -> Direction -> Int -> BitBoard w h a
 shiftD' b dir amount
     = (b `shift` amount)
     .&. complement (wall dir (size b))
 
-shiftD :: (KnownNat w, KnownNat h, Bits a, Num a)
+shiftD :: BitBoardT w h a
        => Direction -> BitBoard w h a -> BitBoard w h a
 shiftD N  b = shiftD' b S  (width b)
 shiftD E  b = shiftD' b W  (-1)
@@ -146,12 +144,12 @@ shiftD NW b = shiftD' b SE (width b+1)
 shiftD SE b = shiftD' b NW (-(width b)-1)
 shiftD SW b = shiftD' b NE (-(width b)+1)
 
-erode :: (KnownNat w, KnownNat h, Bits a, Num a)
+erode :: BitBoardT w h a
       => Direction -> BitBoard w h a -> BitBoard w h a
 erode dir b = b .&. shiftD dir b
 
 {-
-dilate :: (KnownNat w, KnownNat h, Bits a, Num a)
+dilate :: BitBoardT w h a
        => Direction -> BitBoard w h a -> BitBoard w h a
 dilate dir b = b .|. shiftD dir b
 -}
@@ -162,8 +160,7 @@ applyN f k x = iterate f x !! k
 
 -- |Returns True if there are k-in-a-row bits in any direction, orthogonal
 -- or diagonal.
-hasNInRow :: (KnownNat w, KnownNat h, Bits a, Integral a)
-          => BitBoard w h a -> Int -> Bool
+hasNInRow :: BitBoardT w h a => BitBoard w h a -> Int -> Bool
 hasNInRow b k = any (/= 0) eroded
     where
         dirs = [minBound..]
